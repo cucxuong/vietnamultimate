f51541549cdc5cdf9481a523f1080844e32d32cc
hoangaavn <mhoang.tt09@gmail.com>
2023-10-17 17:41:35 +0700

    Update StepGeneral.tsx

--- src/components/Registration/StepGeneral.tsx
+++ src/components/Registration/StepGeneral.tsx
@@ -1,9 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { CalendarBlank, CheckFat, X } from "@phosphor-icons/react";
-import { format } from "date-fns";
-import { useEffect } from "react";
+import { CheckFat, X } from "@phosphor-icons/react";
 import { InputMask } from "@react-input/mask";
+import { useEffect } from "react";
 
 export type StepGeneralData = {
     name: string;
@@ -13,6 +12,7 @@ export type StepGeneralData = {
     email: string;
     nationality: string;
     stayingCountry: string;
+    isStudent: boolean;
 };
 type Props = {
     data: StepGeneralData;
@@ -24,8 +24,30 @@ export default function StepGeneral({ data, validate, onChange, onValidate = (e:
     const handleChange = (prop: string, value: any) => {
         onChange({ ...data, [prop]: value });
     };
+    function toLowerCaseNonAccentVietnamese(str: string) {
+        str = str.toLowerCase();
+        //     We can also use this instead of from line 11 to line 17
+        //     str = str.replace(/\u00E0|\u00E1|\u1EA1|\u1EA3|\u00E3|\u00E2|\u1EA7|\u1EA5|\u1EAD|\u1EA9|\u1EAB|\u0103|\u1EB1|\u1EAF|\u1EB7|\u1EB3|\u1EB5/g, "a");
+        //     str = str.replace(/\u00E8|\u00E9|\u1EB9|\u1EBB|\u1EBD|\u00EA|\u1EC1|\u1EBF|\u1EC7|\u1EC3|\u1EC5/g, "e");
+        //     str = str.replace(/\u00EC|\u00ED|\u1ECB|\u1EC9|\u0129/g, "i");
+        //     str = str.replace(/\u00F2|\u00F3|\u1ECD|\u1ECF|\u00F5|\u00F4|\u1ED3|\u1ED1|\u1ED9|\u1ED5|\u1ED7|\u01A1|\u1EDD|\u1EDB|\u1EE3|\u1EDF|\u1EE1/g, "o");
+        //     str = str.replace(/\u00F9|\u00FA|\u1EE5|\u1EE7|\u0169|\u01B0|\u1EEB|\u1EE9|\u1EF1|\u1EED|\u1EEF/g, "u");
+        //     str = str.replace(/\u1EF3|\u00FD|\u1EF5|\u1EF7|\u1EF9/g, "y");
+        //     str = str.replace(/\u0111/g, "d");
+        str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+        str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+        str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+        str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+        str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+        str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+        str = str.replace(/đ/g, "d");
+        // Some system encode vietnamese combining accent as individual utf-8 characters
+        str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // Huyền sắc hỏi ngã nặng
+        str = str.replace(/\u02C6|\u0306|\u031B/g, ""); // Â, Ê, Ă, Ơ, Ư
+        return str;
+    }
     useEffect(() => {
-        onValidate(data.name !== "" && data.yob !== "" && data.email !== "");
+        onValidate(data.name !== "" && data.yob !== "" && data.email !== "" && data.stayingCountry !== "");
     });
     return (
         <div className="flex flex-col gap-6 -mt-6">
@@ -112,7 +134,7 @@ export default function StepGeneral({ data, validate, onChange, onValidate = (e:
                 </label>
                 <div className="grid grid-cols-1 content-between gap-4 bg-card-foreground bg-opacity-5 backdrop-blur-2xl rounded rounded-b-2xl lg:rounded-bl p-4 lg:p-6">
                     <span className="text-2xl font-medium">Gender{data.gender !== "female" && data.gender !== "male" && <span className="text-rose-500 snap-start">*</span>}</span>
-                    <div className="flex rounded-full overflow-hidden border border-foreground border-opacity-30 h-12">
+                    <div className="flex rounded-full overflow-hidden border border-foreground border-opacity-60 h-12">
                         <button
                             className={`w-full flex items-center justify-center gap-2 px-4 transition-all ease-in-out ${
                                 data.gender === "female" ? "bg-primary bg-opacity-30 duration-200" : "duration-100"
@@ -126,7 +148,7 @@ export default function StepGeneral({ data, validate, onChange, onValidate = (e:
                             )}
                             <span>Female</span>
                         </button>
-                        <div className="border-l border-foreground border-opacity-30"></div>
+                        <div className="border-l border-foreground border-opacity-60"></div>
                         <button
                             className={`w-full flex items-center justify-center gap-2 px-4 transition-all ease-in-out ${
                                 data.gender === "male" ? "bg-primary bg-opacity-30 duration-200" : "duration-100"
@@ -159,7 +181,9 @@ export default function StepGeneral({ data, validate, onChange, onValidate = (e:
                     />
                 </label>
                 <label className="grid grid-cols-1 content-between gap-4 bg-card-foreground bg-opacity-5 backdrop-blur-2xl rounded rounded-b-2xl lg:rounded-bl lg:rounded-r-2xl p-4 lg:p-6">
-                    <span className="text-2xl font-medium">Which country are you staying?</span>
+                    <span className={`text-2xl font-medium ${validate && !data.stayingCountry ? "text-rose-500 snap-start" : ""}`}>
+                        Which country are you staying?{!data.stayingCountry && <span className="text-rose-500">*</span>}
+                    </span>
                     <Input
                         value={data.stayingCountry}
                         clearable
@@ -175,6 +199,36 @@ export default function StepGeneral({ data, validate, onChange, onValidate = (e:
                     />
                 </label>
             </div>
+            {toLowerCaseNonAccentVietnamese(data.stayingCountry.replace(/\W/g, "").toLowerCase()) === "vietnam" && (
+                <div className="grid grid-cols-1 content-between gap-4 bg-card-foreground bg-opacity-5 backdrop-blur-2xl rounded-2xl p-4 lg:p-6">
+                    <span className={`text-2xl font-medium`}>Are you a high school/college student?</span>
+                    <div className="flex rounded-full overflow-hidden border border-foreground border-opacity-60 h-12">
+                        <button
+                            className={`w-full flex items-center justify-center gap-2 px-4 transition-all ease-in-out ${data.isStudent ? "bg-primary bg-opacity-30 duration-200" : "duration-100"}`}
+                            onClick={() => handleChange("isStudent", true)}
+                        >
+                            {data.isStudent && (
+                                <span>
+                                    <CheckFat size={18} weight="fill" />
+                                </span>
+                            )}
+                            <span>Yes</span>
+                        </button>
+                        <div className="border-l border-foreground border-opacity-60"></div>
+                        <button
+                            className={`w-full flex items-center justify-center gap-2 px-4 transition-all ease-in-out ${!data.isStudent ? "bg-primary bg-opacity-30 duration-200" : "duration-100"}`}
+                            onClick={() => handleChange("isStudent", false)}
+                        >
+                            {!data.isStudent && (
+                                <span>
+                                    <CheckFat size={18} weight="fill" />
+                                </span>
+                            )}
+                            <span>No</span>
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
