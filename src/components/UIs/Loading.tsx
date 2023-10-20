2cd814ec7a3fdbf0e11af327586dee56587e2030
hoangaavn <mhoang.tt09@gmail.com>
2023-10-20 16:08:28 +0700

    Update Loading.tsx

--- src/components/UIs/Loading.tsx
+++ src/components/UIs/Loading.tsx
@@ -1,3 +1,3 @@
 export default function Loading() {
-    return <div>Loading...</div>;
+    return <div className="fixed top-0 left-0 w-[100dvw] h-[100dvh] grid place-content-center">Loading...</div>;
 }
