02eb2e5ac1e61125ddc35d16f28b77f612084563
hoangaavn <mhoang.tt09@gmail.com>
2023-11-02 12:04:57 +0700

    Update TournamentLayout.tsx

--- src/components/layouts/TournamentLayout.tsx
+++ src/components/layouts/TournamentLayout.tsx
@@ -11,48 +11,42 @@ import { Button } from "../ui/button";
 import { ArrowLeft } from "lucide-react";
 
 export default function TournamentLayout({ children }: { children: React.ReactNode }) {
-    const { t, i18n } = useAppTranslation();
     const router = useRouter();
     const path = usePathname();
     const [animate, setAnimate] = useState(true);
 
     document.querySelector("body")?.classList.add("dark");
-    const hasLoaded = hasLoadedTranslationResources();
-    if (!hasLoaded) {
-        return <Loading></Loading>;
-    }
 
     return (
-        <>
+        <section className={`grid grid-cols-1 grid-rows-1 gap-12 h-[100dvh] w-[100dvw] overflow-hidden`}>
             <Button
                 onClick={() => {
                     setAnimate(false);
                     setTimeout(() => {
                         setAnimate(true);
                         router.push("/tournament");
-                    }, 400);
+                    }, 300);
                 }}
                 variant={"ghost"}
-                size={"icon-lg"}
-                className={`fixed top-4 left-4 flex-col rounded-full transition-all duration-300`}
+                className={`fixed top-4 left-4 w-[calc(100dvw_-_2rem)] gap-2 justify-between rounded-full transition-all duration-300`}
             >
                 <span>
                     <ArrowLeft size={18} />
                 </span>
+                <span>Vietnam HAT 2023</span>
+                <div className="w-[18px]"></div>
             </Button>
-            <section className={`grid grid-cols-1 grid-rows-1 gap-12 h-[100dvh] w-[100dvw] overflow-hidden`}>
-                <Transition
-                    show={animate}
-                    appear={true}
-                    as={Fragment}
-                    enter="transition-all duration-300 delay-300 ease-in"
-                    leave="transition-all duration-200 ease-out"
-                    enterFrom="scale-110 opacity-0"
-                    leaveTo="scale-90 opacity-0"
-                >
-                    <Main className={`overflow-y-auto overflow-x-hidden`}>{children}</Main>
-                </Transition>
-            </section>
+            <Transition
+                show={animate}
+                appear={true}
+                as={Fragment}
+                enter="transition-all duration-300 delay-300 ease-in"
+                leave="transition-all duration-200 ease-out"
+                enterFrom="scale-110 opacity-0"
+                leaveTo="scale-90 opacity-0"
+            >
+                <Main className={`overflow-y-auto overflow-x-hidden`}>{children}</Main>
+            </Transition>
             <AppBottomBar
                 active={path}
                 onChange={(e) => {
@@ -60,9 +54,9 @@ export default function TournamentLayout({ children }: { children: React.ReactNo
                     setTimeout(() => {
                         setAnimate(true);
                         router.push(e);
-                    }, 400);
+                    }, 300);
                 }}
             />
-        </>
+        </section>
     );
 }
